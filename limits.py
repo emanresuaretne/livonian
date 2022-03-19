62d1671d6c98b714cd51902dd48fc863d9559ac0
retroflexivity <ark.shaldov@gmail.com>
2022-03-19 15:05:49 +0300

    limits visualization

--- limits.py
+++ limits.py
@@ -46,8 +46,35 @@ count(words)
 
 wrfile = "result2.txt"
 with open(wrfile, "w", encoding="utf-8") as wf:
-    wf.write('position\tvowel\tfrequency\n')
+    wf.write('Position\tVowel\tFrequency\n')
     for i in range(len(v_list)):
         for v, freq in v_list[i].items():
             wf.write(f'{i + 1}\t{v}\t{freq}\n')
         wf.write('\n')
+
+
+table = pd.read_csv("result2.txt", sep="\t")
+# print(table)
+# table.head()
+# строим суммирующую таблицу: по строкам - первая гласная, по столбцам - вторая
+table_mt = pd.pivot_table(table, index=["Vowel"], columns=["Position"], values="Frequency", fill_value=0)
+table_mt
+# строим так называемую "heatmap" - клеточка тем интенсивней закрашена, чем частотней пара гласных
+ax = sns.heatmap(table_mt, annot=True, cmap="YlGnBu")
+# notation: "annot" not "annote"
+bottom, top = ax.get_ylim()
+# ax.set_ylim(bottom + 0.0001, top - 0.0001)
+# sns.set(font_scale=2)
+fig = plt.figure()
+fig, ax = plt.subplots(1, 1, figsize=(12, 12))
+heatplot = ax.imshow(table_mt, cmap='BuPu')
+ax.set_xticklabels(table_mt.columns)
+ax.set_yticklabels(table_mt.index)
+
+tick_spacing = 1
+ax.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+ax.set_title("First-second syllable vowels")
+ax.set_xlabel('second_syllable')
+ax.set_ylabel('first_syllable')
+ax.set_ylim(9.0, 0)
