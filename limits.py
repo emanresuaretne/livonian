5f5b55b9b2a795c0f7d3049015aed688cb360df1
emanresuaretne <90254093+emanresuaretne@users.noreply.github.com>
2022-03-20 21:32:38 +0300

    Update limits.py

--- limits.py
+++ limits.py
@@ -1,16 +1,8 @@
 import re
-# from tkinter import *
-# from tkinter.filedialog import askopenfilename
-# %matplotlib inline
 import pandas as pd
-import numpy as np
 import matplotlib.ticker as ticker
-import matplotlib.cm as cm
-import matplotlib as mpl
 import seaborn as sns
 import matplotlib.pyplot as plt
-# root = Tk()
-# root.withdraw()
 
 
 def count(words):
@@ -18,8 +10,8 @@ def count(words):
         vwls = re.findall(vowel, word)
         for i in range(len(vwls)):
             key = vwls[i]
-            # if len(v_list) <= i:
-            #     v_list.append({})
+            if len(v_list) <= i:
+                v_list.append({})
             v_list[i][key] = v_list[i].get(key, 0) + 1
 
     for i in range(len(v_list)):
@@ -33,6 +25,7 @@ word_list = 'livonian_wordlist.txt'
 print(f'Имя файла со словами, по которым будем собирать статистику: {word_list}')
 phonemes = 'livonian_vowels.txt'
 print(f'Имя файла с графемами, которые будем искать в словах из словаря и считать: {phonemes}')
+
 with open(phonemes, encoding="utf-8") as phn:
     vow = phn.read().strip().replace(" ", "|")
 vowel = re.compile(vow)
@@ -54,20 +47,13 @@ with open(wrfile, "w", encoding="utf-8") as wf:
 
 
 table = pd.read_csv("result2.txt", sep="\t")
-# print(table)
-# table.head()
 # строим суммирующую таблицу: по строкам - первая гласная, по столбцам - вторая
 table_mt = pd.pivot_table(table, index=["Vowel"], columns=["Position"], values="Frequency", fill_value=0)
-table_mt
 # строим так называемую "heatmap" - клеточка тем интенсивней закрашена, чем частотней пара гласных
 ax = sns.heatmap(table_mt, annot=True, cmap="YlGnBu")
-# notation: "annot" not "annote"
-bottom, top = ax.get_ylim()
-# ax.set_ylim(bottom + 0.0001, top - 0.0001)
-# sns.set(font_scale=2)
-fig = plt.figure()
+
 fig, ax = plt.subplots(1, 1, figsize=(12, 12))
-heatplot = ax.imshow(table_mt, cmap='BuPu')
+ax.imshow(table_mt, cmap='BuPu')
 ax.set_xticklabels(table_mt.columns)
 ax.set_yticklabels(table_mt.index)
 
@@ -77,4 +63,4 @@ ax.yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
 ax.set_title("First-second syllable vowels")
 ax.set_xlabel('second_syllable')
 ax.set_ylabel('first_syllable')
-ax.set_ylim(9.0, 0)
+plt.show()
